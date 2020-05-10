5a05040bf01186bfedfd9a6827feef4b67caa412
glorelvalle <gloriadelv3@hotmail.com>
2020-05-10 18:01:06 +0200

    limpieza de codigo

--- Proyecto/tsp_mpi.py
+++ Proyecto/tsp_mpi.py
@@ -2,15 +2,17 @@
 # Proyecto de Arquitectura de Sistemas Paralelos                                                   #
 # Paralelización de un Algoritmo Genético aplicado al Problema del Viajante                        #
 #                                                                                                  #
-# tsp_scoop.py: GA para TSP paralelizado con SCOOP                                                 #
+# tsp_mpi.py: GA para TSP paralelizado con MPI                                                     #
 # Pareja 3, Grupo 2461:                                                                            #
 #       Michael Alexander Fajardo                                                                  #
 #       Gloria del Valle Cano                                                                      #
 ####################################################################################################
 from tsp import *
+from mpi4py import MPI
+from mpi4py.futures import MPIPoolExecutor
+from mpi4py.futures import MPICommExecutor
 from functools import partial
 from deap import algorithms, base, creator, tools
-from scoop import futures
 
 num_cities = 30
 cities = generate_cities(num_cities)
@@ -27,40 +29,47 @@ toolbox.register("mate", tools.cxOrdered)
 toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
 
 def create_tour(individual):
-    return [list(cities)[e] for e in individual]
+	return [list(cities)[e] for e in individual]
 
 def evaluation(individual):
-    '''Evaluates an individual by converting it into
-    a list of cities and passing that list to total_distance'''
-    return (total_distance(create_tour(individual)),)
+	'''Evaluates an individual by converting it into
+	a list of cities and passing that list to total_distance'''
+	return (total_distance(create_tour(individual)),)
 
 toolbox.register("evaluate", evaluation)
 
 toolbox.register("select", tools.selTournament, tournsize=3)
 
-#toolbox.register("map", futures.map)
-
-def main():
-    random.seed(4)
-    N_ISLES = 5
-    FREQ = 5
-
-    islands = [toolbox.population(n=100) for i in range(N_ISLES)]
-
-    toolbox.unregister("indices")
-    toolbox.unregister("individual")
-    toolbox.unregister("population")
-
-    toolbox.register("alg_scoop", algorithms.eaSimple, toolbox=toolbox, cxpb=0.8, mutpb=0.2, ngen=5, verbose=False)
-
-    start_time = time.time()
-    for i in range (0, 400, FREQ):
-        results = futures.map(toolbox.alg_scoop, islands)
-        islands = [pop for pop, logbook in results]
-        tools.migRing(islands, 15, tools.selBest)
-
-    print("--- %s seconds ---" % (time.time() - start_time))
-    return islands
 
 if __name__ == "__main__":
-    print(main())
+
+	comm = MPI.COMM_WORLD
+	size = comm.Get_size()
+	rank = comm.Get_rank()
+
+	random.seed(4)
+	N_ISLES = 5
+	FREQ = 5
+
+	if rank == 0:
+		islands = [toolbox.population(n=100) for i in range(size)]
+	else:
+		islands = None
+
+
+	toolbox.unregister("indices")
+	toolbox.unregister("individual")
+	toolbox.unregister("population")
+
+	if rank == 0 :
+		start_time = time.time()
+	for i in range (0, 400, FREQ):
+		islands = comm.scatter(islands,root = 0)
+		resultsT =  algorithms.eaSimple(islands,toolbox=toolbox, cxpb=0.8, mutpb=0.2, ngen=5, verbose=False)
+		results = comm.gather(resultsT,root=0)
+		if rank== 0:
+			islands = [pop for pop, logbook in results]
+			tools.migRing(islands, 15, tools.selBest)
+
+	if rank == 0:
+		print("--- %s seconds ---" % (time.time() - start_time))
